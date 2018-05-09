fda48ea5cbe1eb8791570e2a8bd714350c50a5bb
Blake Hoelle <bhoelle216@gmail.com>
2018-05-08 20:03:55 -0400

    commit

--- Tutorial8-3/Tutorial8-2/Form1.Designer.cs
+++ Tutorial8-3/Tutorial8-2/Form1.Designer.cs
@@ -74,10 +74,12 @@
             // 
             // listBox1
             // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
             this.listBox1.Location = new System.Drawing.Point(67, 77);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(101, 108);
+            this.listBox1.Size = new System.Drawing.Size(101, 100);
             this.listBox1.TabIndex = 5;
             // 
             // Form1
